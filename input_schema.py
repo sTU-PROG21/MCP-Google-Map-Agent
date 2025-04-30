5538e45e6501bb02ce1364b6edd3a283b076b9b9
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-04-30 10:23:34 +0530

    Delete input_schema.py

--- input_schema.py
+++
@@ -1,8 +0,0 @@
-INPUT_SCHEMA = {
-    "user_query": {
-        'datatype': 'STRING',
-        'required': True,
-        'shape': [1],
-        'example': ["Can you find me some Pizza shop in Jorhat Gar ali with good number of reviews?"]
-    }
-}
